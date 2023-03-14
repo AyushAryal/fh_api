de9f48a466eb397a4785398faf685d502b2b7506
Ayush Aryal <ayusharyal100@gmail.com>
2023-03-14 18:40:25 +0545

    populate db

--- administrator/management/commands/populatetestdb.py
+++ administrator/management/commands/populatetestdb.py
@@ -1,9 +1,16 @@
+import urllib.request
+from os.path import isfile
+import csv
+import io
+from PIL import Image
 from datetime import timedelta
 import random
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.core.files.base import ContentFile
+
 
 from store.models import (
     CartItem,
@@ -12,6 +19,7 @@ from store.models import (
     MerchantProfile,
     Item,
     ItemVariant,
+    ItemVariantImage,
     Order,
     OrderStatus,
     Purchase,
@@ -22,6 +30,13 @@ class Command(BaseCommand):
     help = "This command populates the database with default db"
 
     def handle(self, *_, **__):
+        if not self.download_images():
+            return False
+
+        if get_user_model().objects.count() != 0:
+            self.stderr.write(self.style.ERROR("Database is not empty. Aborting."))
+            return False
+
         PASSWORD = "shark@123"
 
         self.create_super_user(email="admin@example.com", password=PASSWORD)
@@ -40,6 +55,39 @@ class Command(BaseCommand):
             user = self.create_customer(details=customer_details)
             self.create_random_transactions(user=user, size=5)
             self.create_random_cart(user=user, size=5)
+
+    def get_image_filename(self, item_id, variant_id):
+        return f"res/downloaded_images/{item_id}_{variant_id}.webp"
+
+    def download_images(self):
+        encountered_errors = False
+        with open("res/vegetables.csv", newline="") as crops_file:
+            crops_reader = csv.reader(crops_file)
+            next(crops_reader, None)
+            for item_id, crop in enumerate(crops_reader):
+                _, __, ___, *variants = crop
+
+                for variant_id, variant in enumerate(variants):
+                    _, url = variant.split(",", 1)
+                    filename = self.get_image_filename(item_id, variant_id)
+                    if not isfile(filename):
+                        try:
+                            response = urllib.request.urlopen(
+                                urllib.request.Request(
+                                    url, headers={"User-Agent": "Mozilla/5.0"}
+                                )
+                            )
+                            image = Image.open(io.BytesIO(response.read()))
+                            image.save(filename)
+                            self.stdout.write(
+                                self.style.SUCCESS(f"Saved image {filename}")
+                            )
+                        except Exception as e:
+                            self.stderr.write(
+                                self.style.ERROR(f"Failed to download image {url}: {e}")
+                            )
+                            encountered_errors = True
+        return not encountered_errors
 
     def create_customer_details(self, name, password):
         return {
@@ -88,63 +136,71 @@ class Command(BaseCommand):
         return user
 
     def create_categories(self):
-        categories = (
-            {"name": "Greens", "description": "Your typical green vegetable"},
-            {"name": "Beans", "description": "Seed that are meant to be eaten"},
-            {
-                "name": "Lentils",
-                "description": "having flat pods containing lens-shaped, edible seeds.",
-            },
-            {"name": "Fruits", "description": "Sweet and high in fructose."},
-            {"name": "Fresh", "description": "Brought in recently"},
-            {"name": "Dried", "description": "Made of synthetic material"},
-        )
-        ret = [Category.objects.create(**category) for category in categories]
-        self.stdout.write(self.style.SUCCESS(f"Added {len(categories)} categories"))
+        categories = []
+        with open("res/vegetables.csv", newline="") as crops_file:
+            crops_reader = csv.reader(crops_file)
+            next(crops_reader, None)
+            for crop in crops_reader:
+                _, __, _categories, *___ = crop
+                categories.extend(x.strip() for x in _categories.split(","))
+        categories_ = [
+            {"name": x, "description": f"This is description for {x}"}
+            for x in set(categories)
+        ]
+        ret = [Category.objects.create(**category) for category in categories_]
+        self.stdout.write(self.style.SUCCESS(f"Added {len(categories_)} categories"))
         return ret
 
     def create_items(self, merchant):
-        items = (
-            {"name": "Tomato", "description": "It is tomato."},
-            {"name": "Potato", "description": "It is potato."},
-            {"name": "Cabbage", "description": "It is cabbage."},
-            {"name": "Peas", "description": "It is peas."},
-            {"name": "Radish", "description": "It is radish."},
-            {"name": "Carrot", "description": "It is carrot."},
-            {"name": "Gourd", "description": "It is gourd."},
-            {"name": "Spinach", "description": "It is spinach."},
-            {"name": "Sweet Potato", "description": "It is sweet potato."},
-            {"name": "Chilli", "description": "It is chilli."},
-        )
-        variants = (
-            {"color": "Yellow"},
-            {"color": "White"},
-            {"color": "Red"},
-            {"color": "Green"},
-        )
+        items = []
+        with open("res/vegetables.csv", newline="") as crops_file:
+            crops_reader = csv.reader(crops_file)
+            next(crops_reader, None)
+            for crop_id, crop in enumerate(crops_reader):
+                item_name, description, categories, *_variants = crop
+                variants = []
+                for variant_id, variant in enumerate(_variants):
+                    name, _ = (x.strip() for x in variant.split(",", 1))
+                    filename = self.get_image_filename(crop_id, variant_id)
+                    variants.append(
+                        {
+                            "color": name,
+                            "rate": random.randint(200, 1000) % 10,
+                            "stock": random.randint(0, 100) % 10,
+                            "images": [filename],
+                        }
+                    )
+                items.append(
+                    {
+                        "name": item_name,
+                        "description": description,
+                        "categories": [x.strip() for x in categories.split(",")],
+                        "variants": variants,
+                    }
+                )
+
         item_instances = []
         variant_instances = []
         for item in items:
+            variants = item.pop("variants")
+            categories = item.pop("categories")
             i = Item.objects.create(
                 user=merchant, **item, subtitle=f"{item['description'][0:50]}..."
             )
-            i.categories.set(random.sample(self.categories, 3))
+            for category_name in categories:
+                i.categories.add(Category.objects.filter(name=category_name).first())
             item_instances.append(i)
 
             for variant in variants:
-                v = ItemVariant.objects.create(
-                    item=i,
-                    rate=random.randint(200, 1000) % 10,
-                    stock=random.randint(0, 100) % 10,
-                    **variant,
-                )
+                images = variant.pop("images")
+                v = ItemVariant.objects.create(item=i, **variant)
                 variant_instances.append(v)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Added {item['name']} variant {variant['color']}"
-                    )
-                )
-
+                for image_path in images:
+                    with open(image_path, "rb") as f:
+                        data = f.read()
+                        ii = ItemVariantImage.objects.create(item_variant=v)
+                        ii.image.save(filename, ContentFile(data))
+            self.stdout.write(self.style.SUCCESS(f"Added {item['name']}"))
         return item_instances, variant_instances
 
     def create_customer(self, details):
